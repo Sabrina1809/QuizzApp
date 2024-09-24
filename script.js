880ddac737dba85da173a98a49ca7bde925c1f2e
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2024-09-24 12:30:50 +0200

    Scripte formatiert

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function init() {
-    fillRandomQuestionsArray();
-    document.getElementById("questionsQty").innerHTML = ` / ${randomIndexArray.length}`;
-    showQuestion()
-}
 let randomIndexArray = [];
-
 let sound_success = new Audio('sound/success.mp3');
 let sound_fail = new Audio('sound/fail.mp3');
 let sound_finish = new Audio('sound/finish.mp3');
 sound_fail.volume = 0.5;
 sound_finish.volume = 0.5;
 sound_success.volume = 0.5;
+let currentQuestion = 0;
+let tryCount = 0;
+
+function init() {
+    fillRandomQuestionsArray();
+    document.getElementById("questionsQty").innerHTML = ` / ${randomIndexArray.length}`;
+    showQuestion()
+}
 
 function fillRandomQuestionsArray() {
     for (let i = 0; i < questions.length; i++) {
@@ -27,8 +29,6 @@ function fillRandomQuestionsArray() {
     console.log(randomIndexArray)
 }
 
-let currentQuestion = 0;
-
 let answers = [
     document.getElementById("answer-1"),
     document.getElementById("answer-2"),
@@ -43,7 +43,13 @@ function showQuestion() {
     document.getElementById("next").style.boxShadow = "none";
     document.getElementById("next").innerHTML = `
         <img src="./img/arrowRight.png" alt="Pfeil nach rechts">`
+    changeDetails(randomIndexArray)
+    rightAnswer(randomIndexArray)
+    currentQuestion = currentQuestion + 1
+    return currentQuestion 
+}
 
+function changeDetails() {
     for (let i = 0; i < answers.length; i++) {
         answers[i].style.pointerEvents = "auto";
         answers[i].classList.remove("rightAnswer");
@@ -53,9 +59,6 @@ function showQuestion() {
         answers[i].textContent = questions[randomIndexArray[currentQuestion]][`answer_${i + 1}`];
         document.getElementById("next").style.pointerEvents = "none";
     }
-    rightAnswer(randomIndexArray)
-    currentQuestion = currentQuestion + 1
-    return currentQuestion 
 }
 
 function rightAnswer(randomIndexArray) {
@@ -69,35 +72,44 @@ function showPercentProgress() {
     document.getElementById("progress-bar").innerHTML = `${percent.toFixed(0)}%`;
 }
 
-let tryCount = 0;
-
 function checkAnswer(e) {
-    console.log(e.target.classList.value)
     let classesClicked = e.target.classList.value;
     if (classesClicked.includes("rightAnswer")) {
         e.target.classList.add("rightAnswerStyle");
-        if (currentQuestion < randomIndexArray.length) {
-            sound_success.play();
-        } else {
-            sound_finish.play();
-        }
-        for (let i = 0; i < answers.length; i++) {
-            answers[i].style.pointerEvents = "none";
-            document.getElementById("next").style.pointerEvents = "auto";
-            document.getElementById("next").innerHTML = `
-                <img src="./img/arrowRightPink.png" alt="Pfeil nach rechts">`
-            document.getElementById("next").style.boxShadow = "0 0 5px rgba(220, 180, 214, 1) inset";
-        }
+        playRightSound()
+        clickedRight();
         if (document.getElementById("currentQuestion").innerHTML == randomIndexArray.length) {
             lastQuestion() 
         }
     } else {
-        e.target.classList.add("wrongAnswerStyle");
-        sound_fail.play();
+        clickedWrong(e)
     }
     tryCount = tryCount + 1
     document.getElementById("try").innerHTML = tryCount
     return tryCount
+}
+
+function playRightSound() {
+    if (currentQuestion < randomIndexArray.length) {
+        sound_success.play();
+    } else {
+        sound_finish.play();
+    }
+}
+
+function clickedWrong(e) {
+    e.target.classList.add("wrongAnswerStyle");
+    sound_fail.play();
+}
+
+function clickedRight() {
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].style.pointerEvents = "none";
+        document.getElementById("next").style.pointerEvents = "auto";
+        document.getElementById("next").innerHTML = `
+            <img src="./img/arrowRightPink.png" alt="Pfeil nach rechts">`
+        document.getElementById("next").style.boxShadow = "0 0 5px rgba(220, 180, 214, 1) inset";
+    }
 }
 
 function lastQuestion() {
